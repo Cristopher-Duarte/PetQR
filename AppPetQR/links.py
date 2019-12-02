d4c9f9005f63cdbdd89539b1702793810dc0770b
Federico <fedeangel2202@gmail.com>
2019-12-02 16:39:39 -0500

    Subiendo todo pero a la hora de abirir los modelos en el servidor no aparecen y ni la menor idea por que xd

--- AppPetQR/links.py
+++ AppPetQR/links.py
@@ -1,6 +1,6 @@
 from AppPetQR.views import *
 
 urlpatterns = [
-
-    path('',include('VeteSoft.links'))
+    #COMENTE ESTO YA QUE DA ERROR A LA HORA DE INICIAR EL SERVIDOR, HASTA QUE SE ARREGLE DEJENLO ASÍ
+    #path('',include('VeteSoft.links'))
 ]
--- AppPetQR/models.py
+++ AppPetQR/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+#-----DATA BASE-----#
+
+
+class Veterinaria(models.model):
+    nombre = models.CharField(max_lenght=40)
+    
+    def __str__(self):
+        return self.nombre
+
+class Usuario(models.model):
+    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
+
+class Mascotas(models.model):
+    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+
+class Recordatorios(models.model):
+    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
+    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
+
+class Notificaciones(models.model):
+    recordatorios = models.ForeignKey('Recordatorios', on_delete=models.CASCADE)
+
+
+#-------------------------VACUNAS Y DESPARACITACIÓN-------------------------#
+
+
+class InfoDesparacitacion(models.model):
+    informacion = models.CharField(max_lenght=40)
+
+    def __str__(self):
+        return self.informacion
+
+class Stickers(models.model):
+    sticker = models.ImageField()
+
+class InfoVacunas(models.model):
+    stickers = models.ForeignKey('Stickers', on_delete=models.CASCADE)
+
+class Detalle(models.model):
+    mascotas = models.ForeignKey('Mascotas', on_delete=models.CASCADE)
+    infovacunas = models.ForeignKey('InfoVacunas', on_delete=models.CASCADE)
+    infodesparacitacion = models.ForeignKey('InfoDesparacitacion', on_delete=models,CASCADE)
+
+
+#-------------------------CONTROLES MEDICOS-------------------------#
+
+
+class MedicoVect(models.model):
+    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
+
+class RecetasMedicas(models.model):
+    mascotas = models.ForeignKey('Mascotas', on_delete=models.CASCADE)
+    medicovect = models.ForeignKey('MedicoVect', on_delete=models.CASCADE)
+
+class DetalleReceta(models.model):
+    recetasmedicas = models.ForeignKey('RecetasMedicas', on_delete=models.CASCADE)
+    nombre = models.CharField(max_lenght=40)
