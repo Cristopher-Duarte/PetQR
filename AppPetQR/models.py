96e3a15ae69400db506d55f24779766104c5456c
luis alejandro <alejandroluis444@gmail.com>
2019-12-03 22:30:25 -0500

    Models

--- AppPetQR/models.py
+++ AppPetQR/models.py
@@ -5,55 +5,127 @@ from django.db import models
 
 class Veterinaria(models.model):
     nombre = models.CharField(max_lenght=40)
+    fecharegistro = models.DataField(auto_now_add = True)
+    logo = models.CharField(max_lenght=40)
+    correocoorporativo = models.CharField(max_lenght=40)
+    estado=models.BooleanField(null=True, default=True)
+    telefono = models.CharField(max_lenght=40)
+    
+
     
     def __str__(self):
         return self.nombre
 
-class Usuario(models.model):
-    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
 
-class Mascotas(models.model):
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
-
-class Recordatorios(models.model):
-    usuario = models.ForeignKey('Usuario', on_delete=models.CASCADE)
-    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
-
-class Notificaciones(models.model):
-    recordatorios = models.ForeignKey('Recordatorios', on_delete=models.CASCADE)
-
-
-#-------------------------VACUNAS Y DESPARACITACIÓN-------------------------#
-
-
-class InfoDesparacitacion(models.model):
-    informacion = models.CharField(max_lenght=40)
+class Especie(models.model):
+    especie = models.CharField(max_lenght=40)
 
     def __str__(self):
-        return self.informacion
+        return self.especie
 
-class Stickers(models.model):
-    sticker = models.ImageField()
-
-class InfoVacunas(models.model):
-    stickers = models.ForeignKey('Stickers', on_delete=models.CASCADE)
-
-class Detalle(models.model):
-    mascotas = models.ForeignKey('Mascotas', on_delete=models.CASCADE)
-    infovacunas = models.ForeignKey('InfoVacunas', on_delete=models.CASCADE)
-    infodesparacitacion = models.ForeignKey('InfoDesparacitacion', on_delete=models,CASCADE)
-
-
-#-------------------------CONTROLES MEDICOS-------------------------#
-
-
-class MedicoVect(models.model):
-    veterinaria = models.ForeignKey('Veterinaria', on_delete=models.CASCADE)
-
-class RecetasMedicas(models.model):
-    mascotas = models.ForeignKey('Mascotas', on_delete=models.CASCADE)
-    medicovect = models.ForeignKey('MedicoVect', on_delete=models.CASCADE)
-
-class DetalleReceta(models.model):
-    recetasmedicas = models.ForeignKey('RecetasMedicas', on_delete=models.CASCADE)
+class Raza(models.model):
     nombre = models.CharField(max_lenght=40)
+
+    def __str__(self):
+        return self.nombre
+
+
+class TipoProducto(models.model):
+    nombre = models.CharField(max_lenght=40)
+    
+
+    def __str__(self):
+        return self.nombre
+
+
+class Genero(models.model):
+    nombre = models.CharField(max_lenght=40)
+
+    def __str__(self):
+        return self.nombre
+
+class GeneroMascota(models.model):
+    nombre = models.CharField(max_lenght=40)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Sticker(models.model):
+    url = models.CharField(max_lenght=40)
+
+    def __str__(self):
+        return self.url
+
+
+class Usuario(models.model):
+    documento = models.CharField(max_lenght=40)
+    nombre = models.CharField(max_lenght=40)
+    apellido = models.CharField(max_lenght=40)
+    fechanacimiento =  models.DataField()
+    fecharegistro = models.DataField(auto_now_add = True)
+    correoelectronico = models.CharField(max_lenght=40)
+    estado=models.BooleanField(null=True, default=True)
+    celular = models.CharField(max_lenght=40)
+
+    fk_veterinaria=models.ForeignKet('Veterinaria', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nombre
+
+
+
+class MedicoVeterinaria(models.model):
+    documento = models.CharField(max_lenght=40)
+    nombre = models.CharField(max_lenght=40)
+    apellido = models.CharField(max_lenght=40)
+    fechanacimiento =  models.DataField()
+    fecharegistro = models.DataField(auto_now_add = True)
+    correoelectronico = models.CharField(max_lenght=40)
+    estado=models.BooleanField(null=True, default=True)
+    celular = models.CharField(max_lenght=40)
+
+    fk_veterinariamodels.ForeignKet('Veterinaria', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nombre
+
+
+class Mascotas(models.model):
+    nombre = models.CharField(max_lenght=40)
+    fechanacimiento =  models.DataField()
+    fecharegistro = models.DataField(auto_now_add = True)
+    estado=models.BooleanField(null=True, default=True)
+
+    fk_especie = models.ForeignKet('Especie', on_delete=models.CASCADE)
+    fk_raza = models.ForeignKet('Raza', on_delete=models.CASCADE)
+    fk_usuario = models.ForeignKet('Usuario', on_delete=models.CASCADE)
+    fk_generomascota = models.ForeignKet('GeneroMascota', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre
+
+
+
+class RecetaMedicas(models.model):
+    fecharegistro = models.DataField(auto_now_add = True)
+
+
+    fk_mascota = models.ForeignKet('Mascotas', on_delete=models.CASCADE)
+    fk_medicoveterinario = models.ForeignKet('MedicoVeterinaria', on_delete=models.CASCADE)
+
+
+    
+
+class Permission(models.model):
+
+    class Meta:
+        permissions=(
+            ('is_usuario', _('Is Usuario')),
+            ('is_admin', _('Is Admin')),
+            ('is_medico', _('Is Medico'))
+
+        )
+
