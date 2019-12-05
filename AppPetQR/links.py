8d7c54d991ad8fd7f23e3177eeecd6e2eb63e0d0
cristopher <cristopherduarte50@gmail.com>
2019-12-05 06:20:10 +0000

    8.1

--- AppPetQR/links.py
+++ AppPetQR/links.py
@@ -1,6 +1,5 @@
 from AppPetQR.views import *
 from django.urls import *
-from AppPetQR import views
 from .models import *
 
 
@@ -9,8 +8,8 @@ from .models import *
 
 urlpatterns = [
     path('Inicio',Inicio,name="inicio"),
-    path('/Vacunas',Vacunas,name="vacuna"),
-    path('/Control',Control,name="control")
+    path('Vacunas',Vacunas,name="vacuna"),
+    path('Control',Control,name="control")
     #path('/login',login.as_view(),name="login"),
 
     #path('Veterinarias',LoginView.as_view(),name="Veterinarias"),
